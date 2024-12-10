93e638d70604bac093f6505511292491577511b2
askldfjafjklsd <167501163+askldfjafjklsd@users.noreply.github.com>
2024-12-10 09:40:17 -0500

    feat: completed field in model

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('id', 'title', 'date')
+        fields = ('id', 'title', 'date', 'completed')
